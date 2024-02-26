31b4cca99fa3b1d4c00111c43f354d1627ed6443
Sean Wadsworth <sw695@sussex.ac.uk>
2024-02-26 15:44:32 +0000

    removed unused import

--- src/components/AddFilm.js
+++ src/components/AddFilm.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from "reactstrap";
 
 function AddFilm({ baseUrl }){
